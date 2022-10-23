ca7113fe4df35bbfe387cf841fd775ae6ef54e08
Flipper <filipe@motionfirestudios.com.au>
2022-10-23 08:12:20 +0800

    overall changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,48 @@
 import React from 'react';
+import { HashRouter, Router, Routes, Route } from 'react-router-dom';
+
 import {
   ApolloClient,
   InMemoryCache,
-  ApolloProvider,
+  ApolloProvider, 
   createHttpLink,
-} from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-// Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
 
 const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
+  <ApolloProvider client={client}>
+    <HashRouter>
         <>
           <Navbar />
           <Routes>
             <Route 
               path='/' 
               element={<SearchBooks />} 
-            />
+              />
             <Route 
               path='/saved' 
               element={<SavedBooks />} 
@@ -53,11 +50,11 @@ function App() {
             <Route 
               path='*'
               element={<h1 className='display-2'>Wrong page!</h1>}
-            />
+              />
           </Routes>
         </>
-      </Router>
-    </ApolloProvider>
+    </HashRouter>
+  </ApolloProvider>
   );
 }
 
